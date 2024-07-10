107127c851e182c9239cbbcf0c14d1acfb9819d1
marothon <marothon@gmail.com>
2024-07-10 10:12:42 +0200

    Add tasks into .js as comments for structure and clarity

--- sinus-minishop/js/script.js
+++ sinus-minishop/js/script.js
@@ -1,1 +1,54 @@
-// Code here!
+/*
+    DOM SELECTION
+*/
+
+// 1.* Byt namn på första hoodien från *Ash* till Potato.
+
+// 2. Byt namn på Home till Start.
+
+// 3. Byt namn på Contact till Mail Us.
+
+// 4. Byt ut informationen om Sinus Hoodie - Fire.
+
+// 5. Byt bakgrundsfärg och text på en knapp.
+
+// 6. Byt bakgrundsfärg på någon av produkterna.
+
+// 7. Byt ut adressen på sidan.
+
+// 8. Byt färg på samtliga <p>.
+
+// 9. Ändra text på samtliga knappar till add to cart.
+
+// 10. Lägg till classen active på menyalternativet home.
+
+// 11. Ta bort classen logo på logotypen.
+
+/*
+    ADD NEW CONTENT
+*/
+
+// 12. Lägg till ett nytt menyalternativ.
+
+/* 13. Lägg till en ny produkt med följande info.
+------------------------------------------------------------------------------------------
+-- tagg    -    beskrivning
+-- img     -    hoodie-forrest.png
+-- h2      -    Sinus Hoodie
+-- h3      -    Forrest
+-- p       -	Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, dolores.
+------------------------------------------------------------------------------------------
+*/
+
+/* 
+
+    EVENTS
+
+*/
+// 14. Lyssna efter ett klick på logotypen (.logo). När den registrerar ett klick skall du console.log:a "found you!";
+
+// 15. Lyssna efter klick på samtliga produkter ( <article>). När den registrerar ett klick skall du console.log:a "Hi, Im article Fire / Ash / Water".
+
+
+
+
